4363238dc449a5e01bcbc7f905d101fe1544739e
szq <814471424@qq.com>
2020-04-17 11:21:28 +0800

    ww

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','weibo App')-laravel 新手入门教程</title>
+    <title>@yield('title','weibo App') - laravel 新手入门教程</title>
 </head>
 <body>
 @yield('content')
