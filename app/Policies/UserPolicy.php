7c046866d2b89b0631490b63e26519fbd61f88a8
szq <814471424@qq.com>
2020-04-20 16:38:13 +0800

    test

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,12 +2,22 @@
 
 namespace App\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
 use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
 {
     use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
 
     public function update(User $currentUser, User $user)
     {
